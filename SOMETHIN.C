41a26bbaf4ce6b0742126f42b64c6935c3479bd4
jasondev-creator <jasonstudy610@gmail.com>
2026-07-20 19:33:28 +0530

    SOMETHING.C

---
+++ SOMETHIN.C
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+char jason_life;
+char obstacles;
+printf("%s\n",jason_life);
+if(jason_life > obstacles) {
+printf("Routine done",jason_life);
+}
+else {
+printf("ok try to take the momentum man",jason_life);
+}
+return 0;
+}
