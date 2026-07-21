afa769e03787c885978cc701dc8936ab2fc1f8d0
Jason <jasonstudy610@gmail.com>
2026-07-21 12:35:08 +0530

    Max_array_element.c

---
+++ Max_array_element.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main () {
+int arr[5],i,max;
+printf("Enter 5 elements:\n");
+for(i = 0; i < 5; i++)
+{
+scanf("%d",&arr[i]);
+}
+max = arr[0];
+for ( i = 0; i < 5; i++)
+{
+if (arr[i] > max)
+{
+max = arr[i];
+}
+}
+printf("Maximum elements:%d\n",max);
+return 0;
+}
