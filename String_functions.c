134532ee64d6a11bc0dc0e32cad42dd623b9298b
Jason <jasonstudy610@gmail.com>
2026-08-23 17:20:21 +0530

    String_functions.c

--- String_functions.c
+++ String_functions.c
@@ -2,22 +2,31 @@
 #include <string.h>
 int main () {
 char str1[100],str2[100],copy[100],concat[200];
-printf("Enter first string:");
+printf("Enter the first string:");
 scanf("%s",str1);
-printf("Enter second string:");
+printf("Enter the second string:");
 scanf("%s",str2);
-printf("\nLength of first string:%d",(int)strlen(str1));
-printf("\nLength of second string:%d",(int)strlen(str2));
+// length
+printf("length of the first string:%d\n",(int)strlen(str1));
+printf("length of the second string:%d\n ",(int)strlen(str2));
+// copy
+strcpy(copy,str1);
+printf("copy of the string:%s\n",copy);
+// concat 
 strcpy(concat,str1);
-strcpy(concat,str2);
-printf("\nconcatenated string:%s",concat);
+strcat(concat,str2);
+printf("concat of the string:%s\n",concat
+);
+// compare
 if(strcmp(str1,str2) == 0)
 {
-printf("\n The strings are equal.");
+printf("the string are equal.\n");
 }
 else
 {
-printf("\n The strings are not equal.");
+printf("the strings are not equal.\n");
 }
 return 0;
 }
+
+
