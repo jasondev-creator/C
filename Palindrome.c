ecb8721417185b8c8168d25296cfe3b8f173962d
N.Jason Augustin <jasonstudy610@gmail.com>
2026-09-23 20:28:52 +0530

    Update Palindrome.c

--- Palindrome.c
+++ Palindrome.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int main() {
-int n,reversed = 0,remainder,original;
+int n,reversed = 0,sum = 0,remainder,original;
 printf("Enter your integer:");
 scanf("%d",&n);
 original = n;
@@ -10,6 +10,8 @@ remainder = n % 10;
 reversed = reversed * 10 + remainder;
 n /= 10;
 }
+printf("sum:%d",sum);
+printf("reverse of the digits:%d",reversed);
 if(original == reversed)
 {
 printf("Its an palindrome %d\n",original);
